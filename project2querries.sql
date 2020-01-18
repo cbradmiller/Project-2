c8a0182dea7c7fd86e1dc9bdfe72e73679929dbe
isaacdolson <30604245+isaacdolson@users.noreply.github.com>
2020-01-18 13:54:45 -0700

    the sql stuff

---
+++ project2querries.sql
@@ -0,0 +1,91 @@
+create table median_data_1990(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+select * from median_data_1990;
+
+create table median_data_1995(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+
+create table median_data_2000(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+create table median_data_2005(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+create table median_data_2010(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+create table median_data_2015(
+	countryname VARCHAR(100),
+	fruit_consumption float,
+	nonstarchy_vegetable_consumption float,
+	beans_and_legumes float,
+	nuts_and_seeds float,
+	unprocessed_red_meat float,
+	sugarsweetened_beverages float,
+	fruit_juices float,
+	protein float,
+	calcium_milligrams float,
+	potassium_milligrams float,
+	total_milk float
+);
+create table us_healthcare_costs_percapita(
+	year_cost VARCHAR(6),
+	percapitaCost FLOAT
+);

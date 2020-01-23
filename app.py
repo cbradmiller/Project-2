0b2e6e39a261efb6c33fc3db19bdbdc22b5d256d
Ryan Kustas <ryankustas@gmail.com>
2020-01-22 19:53:21 -0700

    Testing heroku

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify
+import os
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
-
-#just have to set up the engine. Like below.
-
-# engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+db_url = os.getenv("DATABASE_URL")
+engine = create_engine(db_url)
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+median_data_1990 = Base.classes.median_data_1990
+median_data_1995 = Base.classes.median_data_1995
+median_data_2000 = Base.classes.median_data_2000
+median_data_2005 = Base.classes.median_data_2005
+median_data_2010 = Base.classes.median_data_2010
 
 session = Session(engine)
 
@@ -32,26 +34,26 @@ def home():
 @app.route("/api/v1.0/USHealthCare")
 def healthcareCost():
     #untested
-    healthcareData = session.querry("select * from us_healthcare_costs_percapita;")
+    healthcareData = session.query("select * from us_healthcare_costs_percapita;")
     return(jsonify(healthcareData))
 
 @app.route("/api/v1.0/foodData/<countryName>")
 def foodDataByCountry(countryName):
     #untested
     countryFood = {
-        "1990": session.querry(f"select * from median_data_1990 where lower(countryname) = lower('{countryName}');"),
-        "1995": session.querry(f"select * from median_data_1995 where lower(countryname) = lower('{countryName}');"),
-        "2000": session.querry(f"select * from median_data_2000 where lower(countryname) = lower('{countryName}');"),
-        "2005": session.querry(f"select * from median_data_2005 where lower(countryname) = lower('{countryName}');"),
-        "2010": session.querry(f"select * from median_data_2010 where lower(countryname) = lower('{countryName}');"),
-        "2015": session.querry(f"select * from median_data_2015 where lower(countryname) = lower('{countryName}');")
+        "1990": session.query(f"select * from median_data_1990 where lower(countryname) = lower('{countryName}');"),
+        "1995": session.query(f"select * from median_data_1995 where lower(countryname) = lower('{countryName}');"),
+        "2000": session.query(f"select * from median_data_2000 where lower(countryname) = lower('{countryName}');"),
+        "2005": session.query(f"select * from median_data_2005 where lower(countryname) = lower('{countryName}');"),
+        "2010": session.query(f"select * from median_data_2010 where lower(countryname) = lower('{countryName}');"),
+        "2015": session.query(f"select * from median_data_2015 where lower(countryname) = lower('{countryName}');")
     }
     return(jsonify(countryFood))
 
 @app.route("/api/v1.0/countrynames")
 def getCountryNames():
     #untested
-    namesOfCountries = {"countryNames": session.querry('select countryname from median_data_1990;')}
+    namesOfCountries = {"countryNames": session.query('select countryname from median_data_1990;')}
     return(jsonify(namesOfCountries))
 
 @app.route("/api/v1.0/dataFoodIndex")
